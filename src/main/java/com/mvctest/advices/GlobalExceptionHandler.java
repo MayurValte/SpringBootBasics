89697ec3371d4f5a47645ff22458002437d2c0db
mayur <mayurvalte1999@gmail.com>
2025-07-27 16:13:35 +0530

    Exception Handling till ControllerAdvice

--- src/main/java/com/mvctest/advices/GlobalExceptionHandler.java
+++ src/main/java/com/mvctest/advices/GlobalExceptionHandler.java
@@ -1,18 +1,37 @@
 package com.mvctest.advices;
 
+import com.mvctest.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<APIError> handleResourceNotFoundException(NoSuchElementException exception) {
-        APIError apiError = APIError.builder().httpStatus(HttpStatus.NOT_FOUND).message("Resource not fount").build();
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<APIError> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        APIError apiError = APIError.builder().httpStatus(HttpStatus.NOT_FOUND).message(exception.getMessage()).build();
+        System.out.println("Exception : "+exception.getMessage()+"\n"+ Arrays.toString(exception.getStackTrace()));
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIError> handleInternalSererError(Exception exception){
+        APIError apiError = APIError.builder().httpStatus(HttpStatus.NOT_FOUND).message(exception.getMessage()).build();
+        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIError> handleInputValidationError(MethodArgumentNotValidException exception) {
+        List<String> errors = exception.getBindingResult().getAllErrors().stream().map(error -> error.getDefaultMessage()).collect(Collectors.toList());
+        APIError apiError = APIError.builder().httpStatus(HttpStatus.BAD_REQUEST).message(errors.toString()).build();
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
